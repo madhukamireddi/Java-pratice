d2e9577274c17b3db80b6aa2af1843a4d066dfb4
Madhu <madhukamrieddi2001@gmail.com>
2024-10-12 00:03:43 +0530

    restored version 1

--- addition.java
+++ addition.java
@@ -3,11 +3,6 @@ public class addition {
         int a = 10;
         int b = 20;
         System.out.println(a+b);
-        System.out.println(2*a+2*b);
-        System.out.println("version 2");
-        System.out.println("updating same version 2");
-        System.out.println(a+a-b);
-        System.out.println(b+b-a);
     }
     
 }
--- first.java
+++ first.java
@@ -2,9 +2,5 @@ class first
 {
     public static void main(String[] args) {
         System.err.println("Hello world");
-        System.out.println("version2");
-        System.out.println("updating same version 2");
-        System.out.println("version 3");
-        System.out.println("version 4");
     }
 }
