eaf6c22ec1361c9b5dfa90301984392e753c5f24
Madhu <madhukamrieddi2001@gmail.com>
2024-10-12 01:06:37 +0530

    version 22

--- addition.java
+++ addition.java
@@ -3,6 +3,7 @@ public class addition {
         int a = 10;
         int b = 20;
         System.out.println(a+b);
+        System.out.println("version 22");
     }
     
 }
--- first.java
+++ first.java
@@ -2,5 +2,6 @@ class first
 {
     public static void main(String[] args) {
         System.err.println("Hello world");
+        System.out.println("version 22");
     }
 }
